ab42396ea4771cddf99b525a337715d4cca2d327
JiamingW-Official <123278956+JiamingW-Official@users.noreply.github.com>
2025-12-09 04:32:58 -0500

    chore: formatting cleanup in Mask.js

--- webgl/Mask.js
+++ webgl/Mask.js
@@ -206,8 +206,6 @@ export default class Mask extends Handler {
       // 4. Transition IN (Snap Formation)
       // MAGNETIC SNAP: Boost Attraction + Moderate Brake
       // 4. Transition IN (Snap Formation)
-      // "10x Faster": High Tension Physics
-      // 4. Transition IN (Snap Formation)
       // "10x Faster": Super-Tension + Heavy Brake
       if (this.gpgpu && this.gpgpu.uniforms.velocityUniforms.uAttraction) {
         // 1. GRAVITY WELL: Extreme pull (2.0) -> Strong Hold (0.2)
